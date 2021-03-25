c1f004ffcc26c0906c4cdc0c02b884ebb2f2ef57
Rodrigo Fazenda <rodrigoeidelvein@gmail.com>
2021-03-24 21:14:46 -0300

    Add route info

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/'), (req, res) => {
+    res.send("Bem vindo")
+}
+
+app.get('/info', (req, res) => {
+    res.send('Penshare é um aplicativo para compartilhar e contribuir com ideias')
 })
 
 
