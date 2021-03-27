cde55d68bff59ad6490514f7b75420d8cf15d2f6
Rodrigo Fazenda <rodrigoeidelvein@gmail.com>
2021-03-27 15:52:27 -0300

    Componente TextEditor adicionado e adicionada dependencia ao draft-js

--- react-ui/src/App.tsx
+++ react-ui/src/App.tsx
@@ -1,12 +1,43 @@
 import "tailwindcss/tailwind.css";
 import logo from "./logo.svg";
+import TextEditor from "./TextEditor";
+import HomePage from "./HomePage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="container mx-auto">
-      <img src={logo} width="323" />
-      <h1>Seja bem vindo ao penshare</h1>
-    </div>
+    <Router>
+      <div className="container mx-auto">
+        <div>
+          <nav className="flex flex-row-reverse pt-3">
+            <ul>
+              <li className="inline mr-4 p-3">
+                <Link to="/">Início</Link>
+              </li>
+              <li className="inline mr-4 p-3">
+                <Link to="/editor">Entrar</Link>
+              </li>
+              <li className="inline p-3">
+                <Link to="/sobre">Sobre</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Switch>
+          <Route path="/sobre">
+            Olá, nós somos alunos da FTEC do curso de Análise e Desenvolvimento
+            de Sistemas e estamos criando esse sistema para o nosso trabalho de
+            conclusão.
+          </Route>
+          <Route path="/editor">
+            <TextEditor />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ react-ui/src/HomePage.tsx
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <div className="flex">
+      <div className="max-auto">
+        <h1 className="ml-3">Seja bem vindo ao penshare</h1>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
