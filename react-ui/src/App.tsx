82e1ea7713098f375ffe7781575417deecd19190
Rodrigo Fazenda <rodrigoeidelvein@gmail.com>
2021-03-27 11:07:55 -0300

    Add logo to main page

--- react-ui/src/App.tsx
+++ react-ui/src/App.tsx
@@ -1,9 +1,11 @@
 import "tailwindcss/tailwind.css"
+import logo from './logo.svg'
 
 function App() {
   return (
     <div className="container mx-auto">
-      PENSHARE
+      <img src={logo} />
+      <h1>Seja bem vindo ao penshare</h1>
     </div>
   );
 }
