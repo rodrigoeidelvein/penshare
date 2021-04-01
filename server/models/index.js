a36d4d823de2d4dfe6d2a635dd4f665bd0668d60
Rodrigo Fazenda <rodrigoeidelvein@gmail.com>
2021-04-01 20:19:20 -0300

    add dialecOptions to support Heroku

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,17 @@
 const Sequelize = require('sequelize');
 
+const isDev = process.env.NODE_ENV !== 'production';
+
+const dialectOptions = {}
+
+if (!isDev) {
+    dialectOptions.ssl.require = true;
+    dialectOptions.ssl.rejectUnauthorized = true;
+}
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions
 });
 
 const db = {};
