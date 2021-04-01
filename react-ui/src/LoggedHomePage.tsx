d55f054a83f258285f01a4eb33f3c042aa7e4ff1
Rodrigo Fazenda <rodrigoeidelvein@gmail.com>
2021-04-01 16:31:11 -0300

    Add side navigation for logged user

--- react-ui/src/LoggedHomePage.tsx
+++ react-ui/src/LoggedHomePage.tsx
@@ -1,23 +1,9 @@
-import {
-    GoogleLogout,
-} from "react-google-login";
-import {useContext} from "react";
-import AuthContext from "./contexts/auth";
+import LoggedSideNavigation from "./LoggedSideNavigation";
 
 const LoggedHomePage: React.FC = () => {
-    const clientId = process.env.REACT_APP_GOOGLE_LOGIN_CLIENT_ID as string;
-
-    const {user, logOut} = useContext(AuthContext);
-
-    const successGoogleLogoutResponse = () => {
-        logOut();
-    };
-
     return (
         <div>
-            <div>Olá, {user.firstName}</div>
-            <GoogleLogout clientId={clientId} buttonText="Logout"
-                          onLogoutSuccess={successGoogleLogoutResponse}/>
+            <LoggedSideNavigation />
         </div>
     )
 }
