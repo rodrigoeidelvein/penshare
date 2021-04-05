223b8642d7674ccfbfd5de1340556e62f5b99bc2
Rodrigo Fazenda <rodrigoeidelvein@gmail.com>
2021-04-05 12:00:21 -0300

    Fix ssl connection

--- server/models/index.js
+++ server/models/index.js
@@ -1,23 +1,11 @@
 const Sequelize = require('sequelize');
 
-const isDev = process.env.NODE_ENV !== 'production';
-
-const dialectOptions = {
-    ssl: {}
-}
-
-if (!isDev) {
-    dialectOptions.ssl = false;
-    dialectOptions.ssl.rejectUnauthorized = true;
-}
-
-console.log(process.env.DATABASE_URL)
-
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     protocol: "postgres",
     dialectOptions: process.env.DB_ENABLE_SSL && {
-        require: true
+        require: true,
+        rejectUnauthorized: false
     }
 });
 
