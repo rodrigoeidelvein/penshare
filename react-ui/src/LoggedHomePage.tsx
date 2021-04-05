a0f3f39ccaba47f957363291427a70177609285f
Rodrigo Fazenda <rodrigoeidelvein@gmail.com>
2021-04-05 20:41:30 -0300

    Adicionando algumas funcionalidades pro editor de texto

--- react-ui/src/LoggedHomePage.tsx
+++ react-ui/src/LoggedHomePage.tsx
@@ -1,9 +1,11 @@
 import LoggedSideNavigation from "./LoggedSideNavigation";
+import TextEditor from "./TextEditor";
 
 const LoggedHomePage: React.FC = () => {
     return (
-        <div>
+        <div className="min-h-screen flex flex-row flex-auto flex-shrink-0 antialiased bg-gray-200 text-gray-800">
             <LoggedSideNavigation />
+            <TextEditor />
         </div>
     )
 }
--- react-ui/src/TextEditor.tsx
+++ react-ui/src/TextEditor.tsx
@@ -1,13 +1,69 @@
-import { useState } from "react";
-import { Editor, EditorState } from "draft-js";
+import {useCallback, useState} from "react";
+import {convertFromRaw, convertToRaw, Editor, EditorState, RichUtils} from "draft-js";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBold, faItalic, faListUl, faListOl, faStrikethrough, faUnderline} from "@fortawesome/free-solid-svg-icons";
+import debounce from "lodash.debounce";
 import "draft-js/dist/Draft.css";
+import './MyEditor.css';
 
 function TextEditor() {
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
-  );
+    const [editorState, setEditorState] = useState(() =>
+        EditorState.createEmpty()
+    );
 
-  return <Editor editorState={editorState} onChange={setEditorState} />;
+    const saveToDb = (nextValue: any) => {
+        console.log(JSON.stringify(nextValue))
+    }
+
+    const debouncedSave = useCallback(
+        debounce(nextValue => saveToDb(nextValue), 2000), []
+    )
+
+    const handleEditorState = (editorState: EditorState) => {
+        setEditorState(editorState);
+        const contentState = editorState.getCurrentContent();
+        console.log(editorState.getSelection())
+        debouncedSave(convertToRaw(contentState));
+        // console.log(convertFromRaw(convertToRaw(contentState)))
+    }
+
+    const handleKeyCommand = (command: string, editorState: EditorState) => {
+        const newState = RichUtils.handleKeyCommand(editorState, command);
+
+        if (newState) {
+            setEditorState(newState);
+            return 'handled';
+        }
+
+        return 'not-handled';
+    }
+
+    const handleInlineStyle = useCallback((type: string): void => {
+        setEditorState(RichUtils.toggleInlineStyle(editorState, type));
+    }, [editorState]);
+
+    const handleBlockStyle = useCallback((type: string): void => {
+        setEditorState(RichUtils.toggleBlockType(editorState, type))
+    }, [editorState]);
+
+
+    return (<div className="w-full p-10">
+        <div className="mb-3 w-5/12 flex space-x-4">
+            <button onClick={() => handleInlineStyle("BOLD")}><FontAwesomeIcon icon={faBold}/></button>
+            <button onClick={() => handleInlineStyle("ITALIC")}><FontAwesomeIcon icon={faItalic}/></button>
+            <button onClick={() => handleInlineStyle("UNDERLINE")}><FontAwesomeIcon icon={faUnderline}/></button>
+            <button onClick={() => handleInlineStyle("STRIKETHROUGH")}><FontAwesomeIcon icon={faStrikethrough}/></button>
+            <button onClick={() => handleBlockStyle("unordered-list-item")}><FontAwesomeIcon icon={faListUl}/></button>
+            <button onClick={() => handleBlockStyle("ordered-list-item")}><FontAwesomeIcon icon={faListOl}/></button>
+        </div>
+
+        <Editor
+            editorState={editorState}
+            onChange={handleEditorState}
+            handleKeyCommand={handleKeyCommand}
+            onTab={() => handleBlockStyle("unordered-list-item")}
+        />
+    </div>);
 }
 
 export default TextEditor;
