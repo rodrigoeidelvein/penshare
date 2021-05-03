252665bc453ef249bb765e7968ca9c388a75c4b7
Rodrigo Fazenda <rodrigoeidelvein@gmail.com>
2021-05-03 01:23:28 -0300

    add use_env_variable

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
-
+console.log(process.env.DATABASE_URL)
 module.exports = {
     "development": {
-        "url": process.env.DATABASE_URL,
+        "use_env_variable": process.env.DATABASE_URL,
         "dialect": "postgres"
     },
     "test": {
-        "url": process.env.DATABASE_URL,
+        "use_env_variable": process.env.DATABASE_URL,
         "dialect": "postgres",
         "dialectOptions": {
             ssl: {
