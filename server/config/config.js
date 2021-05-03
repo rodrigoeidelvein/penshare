0c9daeda28b299fc647ac303f9379a1e645180fe
Rodrigo Fazenda <rodrigoeidelvein@gmail.com>
2021-05-03 00:56:53 -0300

    add rejectunauthorized

--- server/config/config.js
+++ server/config/config.js
@@ -9,8 +9,10 @@ module.exports = {
         "url": process.env.DATABASE_URL,
         "dialect": "postgres",
         "dialectOptions": {
-            "ssl": true,
-            rejectUnauthorized: false
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
         }
     },
     "production": {
