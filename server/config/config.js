f6f556594b95c3b97a1411e0b558bf38e412930a
Rodrigo Fazenda <rodrigoeidelvein@gmail.com>
2021-06-11 00:25:54 -0300

    Fix config for sequelize connection

--- server/config/config.js
+++ server/config/config.js
@@ -16,6 +16,7 @@ module.exports = {
     "test": {
         "use_env_variable": process.env.DATABASE_URL,
         "dialect": "postgres",
+        "url": process.env.DATABASE_URL,
         "dialectOptions": {
             ssl: {
                 require: true,
@@ -24,7 +25,14 @@ module.exports = {
         }
     },
     "production": {
+        "use_env_variable": process.env.DATABASE_URL,
+        "dialect": "postgres",
         "url": process.env.DATABASE_URL,
-        "dialect": "postgres"
+        "dialectOptions": {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
 }
